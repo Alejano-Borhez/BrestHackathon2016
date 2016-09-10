edd24153b32db32eaafeab738bc152b7cc81f1a7
Alejano-Borhez <alexander.borohov17@gmail.com>
2016-09-10 14:02:09 +0300

    Dao interface created

--- src/main/java/com/epam/hackathon2016/event/dao/EventDao.java
+++ src/main/java/com/epam/hackathon2016/event/dao/EventDao.java
@@ -1,14 +1,24 @@
 package com.epam.hackathon2016.event.dao;
 
-import com.epam.hackathon2016.event.domain.Event;
+import com.epam.hackathon2016.event.domain.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by alexander on 10.9.16.
  */
 public interface EventDao {
-    Event getEvent(int id);
-    ArrayList<Event> events();
+    Event getEventById(int id);
+    int createEvent(ArrayList<Action> actions, Group group, Date date, String place, double budget);
+    ArrayList<Event> getAllEvents();
+
+    Action getActionById(int id);
+    int createAction(ActionType type, double cost, String desc);
+    ArrayList<Action> getAllActions();
+
+    Survey getSurveyById(int id);
+    int createSurvey(int eventId);
+    ArrayList<Survey> getAllSurveys();
     
 }
