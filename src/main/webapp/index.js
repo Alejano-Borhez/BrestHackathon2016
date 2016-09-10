a7b15ab3bff1d31ac19ff9ef2763cecb502ab2b9
AntonSavitsky <anton.savitsky.1995@gmail.com>
2016-09-11 01:04:06 +0300

    commit1

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
     $("#events-item").click(function() {
         $('#event-list').empty();
         $.get('/application/events', function(events) {
@@ -29,5 +30,25 @@ $( document ).ready(function() {
             }).forEach(function(x) { $('#event-list').append(x); });
         });
     });
+
+    $("#groups-item").click(function() {
+        $('#group-list').empty();
+        $.get('/application/groups', function(groups) {
+            groups.map(function(x) {
+                var $item = $('<div>', {
+                    'class' : 'col-lg-4',
+                    'html': $('<div>', {
+                            'class' : 'caption',
+                            'html' : $('<h3>', { 'html' : x.groupId })
+                                .add( $('<p>').html(x.userList) )
+                    })
+                });
+                return $item;
+            }).forEach(function(x) { $('#group-list').append(x); });
+        });
+    });
+    
     $("#events-item").trigger('click');
+
+    $("#groups-item").trigger('click');
 });
