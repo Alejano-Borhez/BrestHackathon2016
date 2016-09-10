c3de0bd2690b770ada3ced87b742eea682061462
Alejano-Borhez <alexander.borohov17@gmail.com>
2016-09-10 17:41:22 +0300

    Action controller made

--- src/main/java/com/epam/hackathon2016/event/controller/ActionController.java
+++ src/main/java/com/epam/hackathon2016/event/controller/ActionController.java
@@ -2,9 +2,11 @@ package com.epam.hackathon2016.event.controller;
 
 import com.epam.hackathon2016.event.dao.EventDao;
 import com.epam.hackathon2016.event.domain.Action;
+import com.epam.hackathon2016.event.domain.ActionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -28,7 +30,18 @@ public class ActionController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public int createAction(@ModelAttribute Action action) {
+    public int createAction(@RequestParam("actionName") String actionName,
+                            @RequestParam("type") String type,
+                            @RequestParam("costPerUser") double costPerUser,
+                            @RequestParam("actionDescription") String actionDescription
+    ) throws IOException {
+
+        Action action = new Action();
+        action.setActionName(actionName);
+        action.setType(ActionType.valueOf(type));
+        action.setCostPerUser(costPerUser);
+        action.setActionDescription(actionDescription);
+
         return dao.createAction(action);
     }
 }
